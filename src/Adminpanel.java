517adf76683ded4b38506ed151ecc200fe87c033
Thisara Bandara <123467757+thisarabandara2@users.noreply.github.com>
2023-03-17 10:53:43 +0545

    Create the Tecmis
    Use Demo Class for Run the Application

---
+++ src/Adminpanel.java
@@ -0,0 +1,69 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Adminpanel extends JFrame {
+    private JButton userProfilesButton;
+    private JButton norticesButton;
+    private JButton cousersButton;
+    private JButton timeTablesButton;
+    private JPanel adminpanel;
+    private JLabel logoimage;
+
+    public Adminpanel(){
+
+        setVisible(true);
+        setSize(750, 650);
+        setTitle("Admin Panel");
+        add(adminpanel);
+
+
+        userProfilesButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                createUserProfile();
+            }
+        });
+        norticesButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                createNotice();
+            }
+        });
+        cousersButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                createCourse();
+            }
+        });
+        timeTablesButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                createTimeTable();
+            }
+        });
+    }
+
+    private void createTimeTable() {
+
+    }
+
+    private void createCourse() {
+    }
+
+    private void createNotice() {
+    }
+
+    private void createUserProfile() {
+            UserProfiles userpro = new UserProfiles();
+    }
+
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+        logoimage = new JLabel(new ImageIcon("UOR.png"));
+    }
+
+    
+    
+}
