d2bdae32760e08060b29514bcffc944debc76bf7
Thisara Bandara <123467757+thisarabandara2@users.noreply.github.com>
2023-03-19 14:04:29 +0545

    Update

--- src/Demo.java
+++ src/Demo.java
@@ -4,7 +4,6 @@ public class Demo {
     public static void main(String[] args) {
 
         Logging log1 = new Logging();
-        //db connection eka harida
     }
 
     public static void dbConnect() {
