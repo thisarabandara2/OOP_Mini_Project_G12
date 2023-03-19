74c52b62feab2af39f1be5da2fd72ec6b8e7d920
Thisara Bandara <123467757+thisarabandara2@users.noreply.github.com>
2023-03-19 16:16:49 +0545

    update

--- src/UserProfiles.java
+++ src/UserProfiles.java
@@ -1,6 +1,9 @@
+import net.proteanit.sql.DbUtils;
+
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.sql.*;
 
 
 public class UserProfiles extends JFrame {
@@ -9,9 +12,14 @@ public class UserProfiles extends JFrame {
     private JButton createNewUserButton;
     private JPanel userProfile;
     private JPanel displayUsers;
+    private JTable table_1;
+    private JButton removeButton;
+    private JTextField textField1;
 
     public UserProfiles() {
 
+        connect();
+        table_lord();
         setVisible(true);
         setSize(750, 650);
         setTitle("Users");
@@ -22,6 +30,53 @@ public class UserProfiles extends JFrame {
                 // Add user registration part
             }
         });
+
+        removeButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                removeUser();
+            }
+        });
+    }
+
+    public void removeUser() {
+
+
+        }
+
+
+    Connection con;
+    PreparedStatement pst;
+    public void connect(){
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            con = DriverManager.getConnection("jdbc:mysql://localhost/tecmis", "root","root");
+            System.out.println("Success");
+        }
+        catch (ClassNotFoundException ex)
+        {
+            ex.printStackTrace();
+
+        }
+        catch (SQLException ex)
+        {
+            ex.printStackTrace();
+        }
+
+    }
+
+    void table_lord(){
+        try
+        {
+            pst = con.prepareStatement("select user_id,fname,lname,address,email,birth_day,contact_number,user_type from user");
+            ResultSet rs = pst.executeQuery();
+            table_1.setModel(DbUtils.resultSetToTableModel(rs));
+        }
+        catch (SQLException e)
+        {
+            e.printStackTrace();
+        }
+
     }
 
     private void createUIComponents() {
