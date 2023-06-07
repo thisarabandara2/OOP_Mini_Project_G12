d01f4adf2508846bde5c41797050e9a902f59001
yohanvishvajith <yohanvishvajith@gmail.com>
2023-06-07 20:44:26 +0530

    db
    db

--- src/DBConnector.java
+++ src/DBConnector.java
@@ -1,6 +1,7 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class DBConnector {
     private static final String URL = "jdbc:mysql://localhost:3306/tecmis";
@@ -9,5 +10,17 @@ public class DBConnector {
 
     public Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USERNAME, PASSWORD);
+    }
+    public void writeData(String query) {
+
+             try {
+                 Connection connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+                 Statement statement = connection.createStatement();
+                 statement.executeUpdate(query);
+                 connection.close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 }
